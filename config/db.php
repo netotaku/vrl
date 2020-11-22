804ed688abfd9b5e3e52481506102cb4c6da87a0
Nicholas Clarkson <nicholasclarkson@Nicholass-MacBook-Pro.local>
2020-11-22 21:02:57 +0000

    db env

--- config/db.php
+++ config/db.php
@@ -11,11 +11,23 @@ $url = getenv('JAWSDB_MARIA_URL');
 
 $dbparts = parse_url($url);
 
-return array(
-  'server' => $dbparts['host'],
-  'user' => $dbparts['user'],
-  'password' => $dbparts['pass'],
-  'database' => ltrim($dbparts['path'],'/'),
-  'tablePrefix' => 'craft',
-  'port' => $dbparts['port'],
-);
+return [
+  '*' => [
+    'server' => $dbparts['host'],
+    'user' => $dbparts['user'],
+    'password' => $dbparts['pass'],
+    'database' => ltrim($dbparts['path'],'/'),
+    'tablePrefix' => 'craft',
+    'port' => $dbparts['port'],
+  ],
+  'dev' => [
+    'driver' => getenv('DB_DRIVER'),
+    'server' => getenv('DB_SERVER'),
+    'user' => getenv('DB_USER'),
+    'password' => getenv('DB_PASSWORD'),
+    'database' => getenv('DB_DATABASE'),
+    'schema' => getenv('DB_SCHEMA'),
+    'tablePrefix' => getenv('DB_TABLE_PREFIX'),
+    'port' => getenv('DB_PORT')
+  ]
+];
